9628c7a242192298dc081fd228d8336cf0df78a1
Nigil Mohra <82286406+nigilmohra@users.noreply.github.com>
2025-03-21 22:17:54 +0530

    Add files via upload

---
+++ Source/DigitalMod_TB.v
@@ -0,0 +1,95 @@
+// Digital Modulation Schemes - TestBench
+// Nigil
+
+
+// Timescale
+
+`timescale 1ns / 1ps
+
+module DigitalMod_TB();
+
+reg CLK = 1'b0;
+reg RESET;
+reg wEN;
+reg rEN;
+reg dIn;
+
+wire bEmpty;
+wire bFull;
+
+wire send_in;
+wire [15:0] data_pt;
+
+// integer count_i;
+
+// Instantiate 
+    
+synFIFO DUT (CLK, RESET, wEN, rEN, dIn, bFull, bEmpty, send_in, data_pt);
+
+// Clock
+    
+always begin #5 CLK = ~CLK; end
+
+// Write
+    
+task writeData(input test_dIn);
+begin
+    @(posedge CLK);
+        wEN = 1'b1;
+        dIn = test_dIn;
+    @(posedge CLK);
+        $display("Writing Data, Data In : %0d", dIn);
+        wEN = 1'b0;
+end
+endtask
+
+// Read
+    
+task readData();
+begin
+    @(posedge CLK);
+        rEN = 1'b1;
+    @(posedge CLK); 
+        wait(send_in)   
+        rEN = 1'b0;
+end
+endtask
+
+// Stimulus
+initial begin
+    #100 RESET = 1'b0;
+       rEN = 1'b0;
+       wEN = 1'b0;
+        
+    // Test 1 - PASS
+    // Write
+    #10 RESET = 1'b1;
+    @(posedge CLK);
+         
+       
+    $display("Test 1");
+    
+    // Write
+    
+    writeData(0);
+    writeData(1);
+    writeData(0);
+    writeData(1);
+    writeData(0);
+    writeData(1);
+    writeData(0);
+    writeData(1);
+
+    // Read
+        readData();       
+        readData();
+        readData();         
+        readData();      
+        readData();     
+        readData();   
+        readData();    
+        readData(); 
+    $stop;
+end     
+                              
+endmodule
---
+++ Source/SynFIFO.v
@@ -0,0 +1,105 @@
+// Synchronous FIFO
+// Nigil
+
+// Timescale
+
+`timescale 1ns / 1ps
+
+module synFIFO (
+    input CLK, 
+    input RESET,
+    input wEN,      
+    input rEN,  
+    input dIn, 	 
+           
+    output bFull,   		      
+    output bEmpty,  		
+    
+    output send_in,
+    output reg [15:0] data_out      
+);
+
+// Registers
+
+reg reg_f_00;
+reg reg_f_01;
+reg reg_f_02;
+reg reg_f_03;
+reg reg_f_04;
+reg reg_f_05;
+reg reg_f_06;
+reg reg_f_07;
+
+reg [3:0] wPtr;
+reg [2:0] rPtr;
+
+// Wire
+
+wire [15:0] data_pt;
+wire done;
+reg dOut;
+
+assign send_in = done;
+
+// Write
+
+always @(posedge CLK) begin
+    if (!RESET) begin
+        wPtr <= 0;
+    end  
+    else if (wEN && !bFull) begin
+        case (wPtr)
+            0 : reg_f_00 <= dIn;
+            1 : reg_f_01 <= dIn;
+            2 : reg_f_02 <= dIn;
+            3 : reg_f_03 <= dIn;      
+            4 : reg_f_04 <= dIn;
+            5 : reg_f_05 <= dIn;
+            6 : reg_f_06 <= dIn;
+            7 : reg_f_07 <= dIn;   
+        endcase
+        wPtr <= wPtr + 1;
+        if(wPtr == 8)
+            wPtr <= 0;
+    end
+end
+
+// Read
+
+always @(posedge CLK) begin
+    if (!RESET) begin
+        rPtr <= 0;
+    end  
+    else if (rEN && !bEmpty) begin
+        case (rPtr)
+            0 : dOut <= reg_f_00;
+            1 : dOut <= reg_f_01;
+            2 : dOut <= reg_f_02;
+            3 : dOut <= reg_f_03;
+            4 : dOut <= reg_f_04;
+            5 : dOut <= reg_f_05;
+            6 : dOut <= reg_f_06;
+            7 : dOut <= reg_f_07;
+        endcase
+        if(done)
+            rPtr <= rPtr + 1;  
+        else
+            rPtr <= rPtr;
+    end        
+end
+
+always @(posedge CLK)
+begin
+    data_out <= data_pt;
+end    
+    
+// Full and Empty
+
+assign bEmpty   = (rPtr == wPtr);
+assign bFull    = ({~wPtr[3], wPtr[2:0]} == {1'b0, rPtr});
+
+// Instantiate
+
+SineWave SineWave_1 (CLK, RESET, rEN, dOut, done, data_pt); 
+
+endmodule
